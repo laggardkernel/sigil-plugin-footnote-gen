0a7d7695a34023ccd7871f223f21de80bfdacff4
laggardkerenel <laggardkernel@gmail.com>
2018-09-04 14:29:38 +0800

    rename variables and tidy up source
    change note text style as non-order

--- plugin.py
+++ plugin.py
@@ -6,46 +6,51 @@ import re
 
 
 def run(bk):
-    lastid = 0
-    fnid = 0
-    fnid1 = 0
+    note_ref_id = 0
+    note_text_id = 0
 # all xhtml/html files - moves found notes to end of file, insert a link
 # in the text and link to css in the files with notes
     for (id, href) in bk.text_iter():
         html = bk.readfile(id)
         html_original = html
-        found = re.search(r'(?<!<p>)\[\d+\]', html)
-        found1 = re.search(r'\<p\>\[\d+\](.+)\<\/p\>', html)
-        local_fnid1 = 0
-        if found is not None:  # only once for each file with notes
+
+        note_ref = re.search(r'(?<!<p>)\[\d+\]', html)
+        note_text = re.search(r'\<p\>\[\d+\](.+)\<\/p\>', html)
+
+        if note_ref is not None:  # only once for each file with notes
             html = re.sub(
                 r'\<\/head\>', r'<link href="../Styles/footnote.css" rel="stylesheet" type="text/css"/>\n</head>', html)
             html = re.sub(r'\<html.+\>\s*\<head\>',
                         r'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN" xmlns:epub="http://www.idpf.org/2007/ops">\n<head>', html)
             html = re.sub(
                 r'\<\/body\>', r'<aside epub:type="footnote">\n<ol class="duokan-footnote-content">\n</ol>\n</aside>\n</body>', html)
-        while found is not None:
-            fnid = fnid + 1
-            html = re.sub(r'(?<!<p>)\[\d+\]', r'<a class="duokan-footnote" epub:type="noteref" href="' + str(
-                id) + '#fn' + str(fnid) + '" id="fnref' + str(fnid) + '"><img alt="" src="../Images/note.png"/></a>', html, 1)
-            print(id, href, '' + str(fnid) + ':' + found.group(0).strip('[]^'))
-            found = re.search(r'(?<!<p>)\[\d+\]', html)
-        while found1 is not None:
-            fnid1 = fnid1 + 1
-            local_fnid1 = local_fnid1 + 1
-            html = re.sub(r'\<p\>\[\d+\](.+)\<\/p\>', r'', html, 1)
-            html = re.sub(r'\<\/ol\>', r'\n<li class="duokan-footnote-item" id="fn' + str(fnid1) + '">\n<p class="fn"><a href="' +
-                          str(id) + '#fnref' + str(fnid1) + '">[' + str(local_fnid1) + ']</a> ' + found1.group(1).strip('[]^') + '​​​​​​​​</p></li>\n</ol>', html, 1)
-            print(id, href, '' + str(fnid1) + ':' + found1.group(1))
-            found1 = re.search(r'\<p\>\[\d+\](.+)\<\/p\>', html)
-        else:
-            print(id, href, "No notes found")
-        if not html == html_original:
-            bk.writefile(id, html)
-        lastid = id
-# css
-    if fnid > 0:
-        cssdata = '''a.duokan-footnote {
+
+            while note_ref is not None:
+                note_ref_id = note_ref_id + 1
+                html = re.sub(r'(?<!<p>)\[\d+\]', r'<a class="duokan-footnote" epub:type="noteref" href="' + str(
+                    id) + '#fn' + str(note_ref_id) + '" id="fnref' + str(note_ref_id) + '"><img alt="" src="../Images/note.png"/></a>', html, 1)
+                print(id, href, '' + str(note_ref_id) + ':' + note_ref.group(0).strip('[]^'))
+                note_ref = re.search(r'(?<!<p>)\[\d+\]', html)
+
+            while note_text is not None:
+                note_text_id = note_text_id + 1
+                html = re.sub(r'\<p\>\[\d+\](.+)\<\/p\>', r'', html, 1)
+                html = re.sub(r'\<\/ol\>', r'\n<li class="duokan-footnote-item" id="fn' + str(note_text_id) + '">\n<p class="fn"><a href="' +
+                              str(id) + '#fnref' + str(note_text_id) + '">◎</a>' + note_text.group(1).strip('[]^') + '​​​​​​​​</p></li>\n</ol>', html, 1)
+                print(id, href, '' + str(note_text_id) + ':' + note_text.group(1))
+                note_text = re.search(r'\<p\>\[\d+\](.+)\<\/p\>', html)
+            else:
+                print(id, href, "No notes found")
+
+            if not html == html_original:
+                bk.writefile(id, html)
+
+    insert_note_css(bk)
+    return 0
+
+
+def insert_note_css(bk):
+    cssdata = '''a.duokan-footnote {
   line-height: 1;
   text-decoration: none;
   height: auto;
@@ -86,13 +91,13 @@ p.fn {
 p.fn a {
   color: #000000;
   text-decoration: none;
-  margin-left: -1.5em;
+  margin-left: -1em;
 }
 '''
-        basename = "footnote.css"
-        uid = "footnotecss"
-        mime = "text/css"
-        bk.addfile(uid, basename, cssdata, mime)
+    basename = "footnote.css"
+    uid = "footnotecss"
+    mime = "text/css"
+    bk.addfile(uid, basename, cssdata, mime)
     return 0
 
 
